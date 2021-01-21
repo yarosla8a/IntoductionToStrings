0d517d6a3c1f9096c035bf9ce6a36b79168762dc
Yaroslava <33331868+yarosla8a@users.noreply.github.com>
2021-01-21 22:05:14 +0200

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,42 @@
+print("Hello World!")
+x ="hello"
+type_of_variale = type(x)
+print(type_of_variale)
+
+greeting = "Hello"
+first_name = "Yasay"
+last_name = "Piskavola"
+exclamation_sing="!"
+white_space =" "
+whole_sentence =greeting+white_space+first_name+" "\
+                +last_name+exclamation_sing
+
+print(whole_sentence)
+
+
+long_string='This is the long string'
+print(long_string)
+
+#Escaping
+some_string ='I\'m a programmer'
+print(some_string)
+another_string="I want to learn \"Pytho\""
+print(another_string)
+
+
+string_with_new_lines ="Hello!\n  \r My name is Yasay"
+print(string_with_new_lines)
+
+numbers ="1\t23\t4567"
+print(numbers)
+
+some_text="\tHello! \n\t I1m very glad to see you!"
+print(some_text)
+
+#Triple quotes
+string_whith_triple_quotes=""""This is 
+text with
+"triple
+quotes" """
+print(string_whith_triple_quotes)
+
